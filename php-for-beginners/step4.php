2ee92b6e65d1a90d4a1f5d689c16bc6a5a7d77f3
Vincent Chiu <vincent@datanerds.com>
2016-08-08 12:25:43 -0700

    basic php stuff

--- php-for-beginners/step4.php
+++ php-for-beginners/step4.php
@@ -5,6 +5,21 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 
+	<style>
+		
+		header {
+
+
+			background: #e3e3e3;
+
+			padding: 2em;
+
+			text-align: center;
+
+		}
+
+
+	</style>
 
 </head>
 
@@ -13,6 +28,21 @@
 
 hello
 
+	<header>
+		<h1>
+
+
+		<?php
+
+		$greeting = "hey there, ";
+		$name = $_GET['name'];
+
+		echo $greeting . $name;
+		?>
+
+		</h1>
+
+	</header>
 </body>
 
 
