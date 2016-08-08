72fa005b2540e471158458c9b30e7d4a18d980d3
Vincent Chiu <vincent@datanerds.com>
2016-08-08 12:43:20 -0700

    done

--- php-for-beginners/step4.php
+++ php-for-beginners/step4.php
@@ -31,7 +31,12 @@ hello
 	<header>
 		
 
-		<h1> <?php echo "Hello, " . htmlspecialchars($_GET['name']); ?> </h1>
+		<h1> <?php 
+		echo "Hello, " . htmlspecialchars($_GET['name']);
+
+		echo "hi" . $_GET['greeting'];
+
+		 ?> </h1>
 
 	</header>
 </body>
