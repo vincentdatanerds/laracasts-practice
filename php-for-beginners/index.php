8017b13336aaaff3a5bbb5a520f4b7b46e182681
Vincent Chiu <vincent@datanerds.com>
2016-08-08 15:24:11 -0700

    finished night club age check function

--- php-for-beginners/index.php
+++ php-for-beginners/index.php
@@ -13,3 +13,30 @@ function dd($datasoup){
 
 
 dd('hello world');
+
+
+dd($animals);
+
+
+dd(['wow', 'cool']);
+
+
+
+
+function checkAgeForNightClub($age){
+	if($age >= 21){
+		echo "welcome in, you are old enough. 
+		";
+	} else {
+		echo "go away, you are not old enough
+		";
+	}
+}
+
+
+
+checkAgeForNightClub(4);
+checkAgeForNightClub(20);
+checkAgeForNightClub(21);
+checkAgeForNightClub(22);
+checkAgeForNightClub(30);
